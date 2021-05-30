823eabc3aebea2cc7e73d411aac74ba3c0d17d11
sekiguchi0215 <dsphere5296@gmail.com>
2021-05-30 17:44:49 +0900

    Ruby課題（応用）２０

--- task.rb
+++ task.rb
@@ -229,12 +229,34 @@ end
 
 class UserQ20
   # 以下に回答を記載
+  attr_reader :name, :age
 
+  def initialize(name:, age:)
+    @name = name
+    @age = age
+  end
 end
 
 class Zoo
   # 以下に回答を記載
+  attr_reader :entry_fee
+  def initialize(name:, entry_fee:)
+    @name = name
+    @entry_fee = entry_fee
+  end
 
+  def info_entry_fee(users)
+    case users.age
+    when 3
+      puts "#{users.name}さんの入場料金は #{entry_fee[:infant]} 円です。"
+    when 10
+      puts "#{users.name}さんの入場料金は #{entry_fee[:children]} 円です。"
+    when 32
+      puts "#{users.name}さんの入場料金は #{entry_fee[:adult]} 円です。"
+    when 108
+      puts "#{users.name}さんの入場料金は #{entry_fee[:senior]} 円です。"
+    end
+  end
 end
 
 def q20
